a6c16f207e09478e1c7c6cfee9b28ca4bca607e9
Murat Bilgel <bilgelm@gmail.com>
2018-05-04 10:16:31 -0400

    updated notebook

--- work/docker_command.sh
+++ work/docker_command.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 8888:8888 -v /Users/bilgelm/Research/MELD_dev/MELD:/home/jupyteruser/meld -v /Users/bilgelm/Research/MELD_dev/work:/home/jupyteruser/work mymeld
+docker run --rm -p 8888:8888 -v /Users/bilgelm/Research/MELD_dev/MELD:/home/jupyteruser/meld mymeld
