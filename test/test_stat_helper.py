5ffc4d25f4e15c2359e86268bc8527c857da8de7
shotgunosine <adenosine@gmail.com>
2018-06-07 22:58:35 -0400

    add function for calculating bootstrap statistic

---
+++ test/test_stat_helper.py
@@ -0,0 +1,18 @@
+import numpy as np
+from meld import stat_helper as msh
+
+def test_boot_stat():
+    stat = np.array([[[1.], [10.]], [[2.], [20.]], [[5.], [50.]]])
+    sterr = np.array([[[0.1],[1]], [[0.4], [4]], [[2],[20]]])
+
+    expected = np.array(      [[[ 10. ],
+                                [ 10. ]],
+                        
+                               [[  2.5],
+                                [  2.5]],
+
+                               [[  2. ],
+                                [  2. ]]], dtype=float)
+    observed = boot_stat(stat, sterr)
+    assert (observed == expected).all()
+    assert observed.dtype == expected.dtype
